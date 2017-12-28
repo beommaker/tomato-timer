432a17bf82ef8da741bdeafe9e2b5a56a4cc1270
beommaker <beom.maker@gmail.com>
2017-12-28 16:59:53 +0900

    feat(timer): add-interval-function
    https://app.asana.com/0/483222130713609/496250181264309

---
+++ components/Timer/presenter.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+import Button from "../Button";
+
+
+class Timer extends Component {
+
+    componentWillReceiveProps(nextProps) {
+        const currentProps = this.props;
+        if(!currentProps.isPlaying && nextProps.isPlaying){
+            // start the interval
+            const timerInterval = setInterval(() => {
+                currentProps.addSecond()
+            }, 1000);
+            this.setState({
+                timerInterval
+            })
+        } else if(currentProps.isPlaying && !nextProps.isPlaying){
+            // stop the interval
+            clearInterval(this.state.timerInterval);
+        }
+      }
+
+    render() {
+        console.log(this.props)
+        const {
+            isPlaying,
+            elapsedTime,
+            timerDuratio,
+            startTimer,
+            restartTimer,
+            addSecond   
+        } = this.props;
+        return (
+            <View style={styles.container}>
+                <StatusBar barStyle={"light-content"} />
+                <View style={styles.upper}>
+                    <Text style={styles.time}>25:00</Text>
+                </View>
+                <View style={styles.lower}>
+                    {!isPlaying && <Button iconName="play-circle" onPress={startTimer} />}
+                    {isPlaying && <Button iconName="stop-circle" onPress={restartTimer} />}
+                </View>
+            </View>
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#CE0B24"
+    },
+    upper: {
+        flex: 2,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    lower: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    time: {
+        color: "white",
+        fontSize: 120,
+        fontWeight: "100",
+    },
+});
+
+export default Timer;
